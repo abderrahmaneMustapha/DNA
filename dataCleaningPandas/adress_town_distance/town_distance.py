efddadfbf3bc963d5fcd6f44689479eb48b06211
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2019-12-30 20:46:10 +0100

    calculate distance for each town

--- dataCleaningPandas/adress_town_distance/town_distance.py
+++ dataCleaningPandas/adress_town_distance/town_distance.py
@@ -1,5 +1,19 @@
 import pandas as pd
+from geopy.distance import great_circle
 
 data  =  pd.read_csv("AllStudent.csv")
 data  =  data.filter(['N°Ins','Adresse']).drop_duplicates()
-pd.DataFrame(data).to_csv('dp_adress.csv',index=False)
+pd.DataFrame(data).to_csv('clean_adress.csv',index=False)
+
+towns = pd.read_csv("town.csv")
+
+from math import radians
+#tiaret,35.3451336,1.2671041,
+for i in range(len(towns)):
+    tiaret = (35.3451336,1.2671041)
+    town= (float(towns['lat'][i]), float(towns['long'][i]))
+    towns['distance'][i] = great_circle(tiaret,town).km
+
+pd.DataFrame(towns).to_csv('town_distance.csv',index=False)
+
+  
