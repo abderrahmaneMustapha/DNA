b8f8c7388ea320b22d1ed44ce653c5d8c6abc4c7
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-01-16 00:04:49 +0100

    search student'

--- react app/dna/src/profile/Profile.js
+++ react app/dna/src/profile/Profile.js
@@ -3,18 +3,51 @@ import './profile.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 class Profile extends React.Component {
   constructor(){
     super();
     this.state = {
-      students : [],
-      default_student: ''
+
+      default_student: 14011599308
     }
   }
-  componentDidMount() {
-  
+  getStudents = (result)=>{
+     var temp = []
+     for(var i=0;i< result.length; i++){
+       temp.push(result[i]['student'])
+       
+     }
+     console.log(temp[0])
 
+     this.setState({
+      students : [...temp],
+     })
+  }
+
+  connectToPerformance = ()=>{
+    
+  fetch('http://127.0.0.1:8000/performance/').
+    then(res => res.json()).
+    then(
+      result =>{
+        this.getStudents(result)    
+      }
+    )
+  }
+
+  connectToCourses = ()=>{
+    
+    fetch('http://127.0.0.1:8000/courses/').
+      then(res => res.json()).
+      then(
+        result =>{
+          this.getSemesterCourses(result)    
+        }
+      )
+    }
+
+  componentDidMount() {
+    this.connectToPerformance()
   }
   render(){
     return (
@@ -24,7 +57,15 @@ class Profile extends React.Component {
           /* the header we can choose a student */
         }
 
-        <header>a</header>
+        <header className="input-group">
+        <div className="col-md-6">
+        <input className="form-control" type="text" value={this.state.default_student}/>
+          <input  className=" btn-orange" type="button"/>
+        </div>
+      
+          
+      
+        </header>
 
         { 
           /* here you can see a radar about student performance in courses */
