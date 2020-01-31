c6534f0371f00111d337b1eda844ec18e0bec032
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-01-31 23:04:58 +0100

    try svm on problem 04

--- machine learning/problem_04/bac_path_on_student_result.py
+++ machine learning/problem_04/bac_path_on_student_result.py
@@ -1,29 +1,77 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+#import packages
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 
 
+# %%
 data = pd.read_csv("student_bac_path.csv")
-X = data["bac_avg"]  
-Y = data["bac_path"]
 
-plt.scatter(X,Y)
-plt.show()
-from sklearn import preprocessing
-le = preprocessing.LabelEncoder()
-X = le.fit_transform(X)
-Y = le.fit_transform(Y)
+#drop student id and bac id
+data = data.drop(['bac_id','student_id'], axis=1)
+data
 
+
+# %%
+data.info()
+
+
+# %%
+Y  =  data['status'].copy()
+Y
+
+
+# %%
+
+X= data.loc[:, data.columns != 'status']
+
+
+X.info()
+
+
+# %%
+#convert X axis to numeric
+X['gender'],_  = pd.factorize(X['gender'])
+X['level'],_  = pd.factorize(X['level'])
+X['repeated'],_  = pd.factorize(X['repeated'])
+X['study_path'],_  = pd.factorize(X['study_path'])
+X['scholar_year'],_  = pd.factorize(X['scholar_year'])
+X['bac_year'],_  = pd.factorize(X['bac_year'])
+X['bac_path'],_  = pd.factorize(X['bac_path'])
+X['bac_feedback'],_  = pd.factorize(X['bac_feedback'])
+X['bac_avg'],_  = pd.factorize(X['bac_avg'])
+X.info()
+
+
+# %%
+#convert Y axis to numeric
+Y,_  = pd.factorize(Y)
+Y
+
+
+# %%
+#slplit data to train and test
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state=0)
 
 
-from sklearn.neighbors import KNeighborsClassifier
-logreg = KNeighborsClassifier(n_neighbors=1)
-logreg.fit(X, Y)
+# %%
+#train
+from sklearn import svm
+clf = svm.SVC(kernel="linear", C=0.5)
+clf.fit(X_train,y_train)
 
-print(logreg.score(X, Y))
+
+# %%
+#predict
+pred = clf.predict(X_test)
+accuracy = accuracy_score(y_test, pred)
+print(" accuracy ",accuracy)
 
 
+# %%
 
