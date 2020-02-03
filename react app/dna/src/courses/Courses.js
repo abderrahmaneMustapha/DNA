3cf06997b93cc22828c070d580d175d5ecffccf3
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-02-03 23:28:56 +0100

    start with course template

--- react app/dna/src/courses/Courses.js
+++ react app/dna/src/courses/Courses.js
@@ -3,22 +3,61 @@ import './courses.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Card from 'react-bootstrap/Card';
 
 class Courses extends React.Component {
   constructor(){
     super();
     this.state = {
-      students : []
+      courses : []
     }
   }
+  getcourses = (result)=>{
+     this.setState({
+       courses : [...result]
+     })
+  }
+  connectToCourses = ()=>{
+    fetch('http://127.0.0.1:8000/courses/').
+    then(res => res.json()).
+    then(
+      result =>{
+        this.getcourses(result)     
+      }
+    )
+  }
   componentDidMount() {
-  
+    this.connectToCourses()
+  }
+
+  courseList = ()=>{
+  let courses = this.state.courses
+   return courses.map((course)=>
+   <Card  >
+   <Card.Body>
+     {course.cours_id}
+        
+   </Card.Body>
+</Card>
+   )
   }
   render(){
+
+    let courseList  = this.courseList()
     return (
       
       <div className="Courses">
+      
+        <header className="courses-header">
+          <h1>ALL IBN KHALDOUN UNIVERSITY COURSES,
+   STATS AT YOUR FINGERTIPS</h1>
+        </header>
 
+        <section>
+          <div className="all-courses-list">
+            {courseList}
+          </div>
+        </section>
       
       </div>
     );
